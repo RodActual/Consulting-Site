68a24d9ae33feb704322ca3fda4d28f58e5e7bf6
Anthony <rodrig99@miamioh.edu>
2026-05-12 12:07:33 -0400

    Added speed insight package

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,5 +1,6 @@
 import type { Metadata } from "next";
 import { Analytics } from "@vercel/analytics/next"
+import { SpeedInsights } from '@vercel/speed-insights/next';
 import "./globals.css";
 
 
@@ -25,6 +26,7 @@ export default function RootLayout({
       <body className="antialiased">
         {children}
         <Analytics />
+        <SpeedInsights />
       </body>
     </html>
   );
