458a9079bb643fab57a86819a115e7d07b909ed3
Anthony <rodactual@proton.me>
2026-05-13 12:23:53 -0400

    Updated client portal data for JW Cleaning

--- lib/portalData.ts
+++ lib/portalData.ts
@@ -2,10 +2,9 @@ export const clientPortals: Record<string, any> = {
   "r04cymNBvi": {
     name: "JW Cleaning",
     links: [
-      { label: "Encrypted File Vault", url: "https://drive.proton.me/..."},
-      { label: "Live KPI Dashboard", url: "https://lookerstudio.google.com/..."},
-      { label: "Access Keys", url: "https://pass.proton.me/..."},
-      { label: "System Architecture", url: "..."},
+      { label: "Encrypted File Vault", url: "https://drive.proton.me/urls/5V9HGA90A0#0vh65YcRHebu"},
+      { label: "Access Keys", url: "https://pass.proton.me/u/0/share/l6NaT3RIWpdqSdEKzD02RoSwEEOcPoAAjOuJhJ2tRvI0vx47HBnVYV6bhBmjq4UxZ0FcmIhyGkJjuLii9efl6w==?filters=eyJzZWFyY2giOiIiLCJzb3J0IjoicmVjZW50IiwidHlwZSI6IioiLCJzZWxlY3RlZFNoYXJlSWQiOiJsNk5hVDNSSVdwZHFTZEVLekQwMlJvU3dFRU9jUG9BQWpPdUpoSjJ0UnZJMHZ4NDdIQm5WWVY2YmhCbWpxNFV4WjBGY21JaHlHa0pqdUxpaTllZmw2dz09In0%3D"},
+      { label: "Active Invoice and Payment", url: "https://payments.found.com/invoices/invoice_fHEtXuo5D4nj"},
     ],
     agreement: {
       status: "Active",
@@ -14,4 +13,5 @@ export const clientPortals: Record<string, any> = {
     }
   },
   // Add more clients as you onboard them
+  //{ label: "Live KPI Dashboard", url: "https://lookerstudio.google.com/..."},
 };
